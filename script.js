6675271373fa73201822ee66c2ce049d4c516875
Adelina <adelina.sahlean@gmail.com>
2021-04-11 14:30:19 +0200

    basic design

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", loadSVG);
+
+
+function loadSVG() {
+    console.log("loadSVG");
+
+    fetch("avatar-01.svg")
+    .then( response => response.text() )
+    .then( svgData => {
+        console.log("SVG loaded");
+
+        // TODO: put the SVG into the DOM
+        document.querySelector("section").innerHTML = svgData;
+        // TODO: Start the animation
+    })
+    }
