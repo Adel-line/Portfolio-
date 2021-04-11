99e859c71429a413320af16f8faf43e67b2f52bd
Adelina <adelina.sahlean@gmail.com>
2021-04-11 18:27:29 +0200

    getting there

--- script.js
+++ script.js
@@ -13,4 +13,14 @@ function loadSVG() {
         document.querySelector("section").innerHTML = svgData;
         // TODO: Start the animation
     })
+
+    fetch("organic-01.svg")
+    .then( response => response.text() )
+    .then( svgData => {
+        console.log("SVG loaded");
+
+        // TODO: put the SVG into the DOM
+        document.querySelector("#organic").innerHTML = svgData;
+        // TODO: Start the animation
+    })
     }
