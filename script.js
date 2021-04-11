ec61119bacb841f0058adaa7535c1faebd5c3cdf
Adelina <adelina.sahlean@gmail.com>
2021-04-11 20:46:40 +0200

    styling almost done

--- script.js
+++ script.js
@@ -12,15 +12,18 @@ function loadSVG() {
         // TODO: put the SVG into the DOM
         document.querySelector("section").innerHTML = svgData;
         // TODO: Start the animation
-    })
+    });
 
-    fetch("organic-01.svg")
-    .then( response => response.text() )
-    .then( svgData => {
-        console.log("SVG loaded");
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to("#p1", {duration: 0.5, x: "35vw", scrollTrigger:{trigger:"#p1"},  toggleActions:"play pause resume reset"});
 
-        // TODO: put the SVG into the DOM
-        document.querySelector("#organic").innerHTML = svgData;
-        // TODO: Start the animation
-    })
+   
+    gsap.to("#p2", {duration: 1.5, x: "48vw", scrollTrigger:{trigger:"#p2"},  toggleActions:"play pause resume reset"});
+
+    
+    
+    
     }
+
+  
+
